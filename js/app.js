af39eb9165254ad6033882aaa876ef8f8baa3c0f
Chad Rich <surfinmet99@yahoo.com>
2015-07-05 16:53:27 -0400

    initial jquery

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
 /*js*/
+$(document).ready(function(){
+	$('#btn_createList').click(function(){
+		$('.ul_current').append($('<li>', {
+			text: $('#item').val()
+		}));
+	});
+});
+
+$(document).ready(function(){
+	$('.items').mouseenter(function(){
+		$('.delete').show();
+		$('.check').show();
+	})
+
+	$('.items-checked').mouseenter(function(){
+		$('.delete').show();
+		$('.check').show();
+	})
+
+	$('.items').mouseleave(function(){
+		$('.delete').hide();
+		$('.check').hide();
+	});
+
+	$('.items-checked').mouseleave(function(){
+		$('.delete').hide();
+		$('.check').hide();
+	});
+
+	$('.items').mousedown(function(){
+		$('.items-checked').show();
+		$('.items').hide();
+	})
+
+	$('.items-checked').mousedown(function(){
+		$('.items').show();
+		$('.items-checked').hide();
+	});
+});
